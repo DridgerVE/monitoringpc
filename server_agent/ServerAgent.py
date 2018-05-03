7abf30fad98ba19c42266435d5931c26cfeda4d6
Виталий Е. Дридгер <dridgerve@s1212.ru>
2018-05-03 11:24:00 +0300

    Fix ServerAgent.py

--- server_agent/ServerAgent.py
+++ server_agent/ServerAgent.py
@@ -20,50 +20,6 @@ db_queue = queue.Queue()
 result_queue = queue.Queue()
 
 
-class MakeMetricsWorker(threading.Thread):
-    """Класс-поток для записи данных в словарь метрик"""
-
-    def __init__(self, metrics, result_q, q_timeout, sleep_time, **kwargs):
-        super().__init__(**kwargs)
-        self.result_q = result_q
-        self.timeout = q_timeout
-        self._stopped = False
-        self._seconds = 0
-        self._sleep = sleep_time
-        self.metrics = metrics
-
-    def stop(self):
-        self._stopped = True
-
-    def check_state(self):
-        for key in self.metrics:
-            if self.metrics[key]["state"] == STATE_OFF:
-                continue
-            if self.metrics[key]["state"] == STATE_UNKNOWN:
-                if datetime.datetime.now().timestamp() - self.metrics[key]["time_last_action"] > STATE_TIMEOUT_OFF:
-                    self.metrics[key]["state"] = STATE_OFF
-            else:
-                if datetime.datetime.now().timestamp() - self.metrics[key]["time_last_action"] > STATE_TIMEOUT_UNKNOWN:
-                    self.metrics[key]["state"] = STATE_UNKNOWN
-
-    def run(self):
-        while not self._stopped:
-            time.sleep(self._sleep)
-            self._seconds += self._sleep
-            if self._seconds > 60:
-                self._seconds = 0
-                self.check_state()
-            try:
-                data = self.result_q.get(block=True, timeout=self.timeout)
-                self.metrics[data["uid"]] = data.copy()
-                self.result_q.task_done()
-            except queue.Empty:
-                continue
-            except:
-                self.result_q.task_done()
-                continue
-
-
 class DBWorker(threading.Thread):
     """Класс-поток для записи данных в БД"""
 
@@ -187,9 +143,9 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
 
 class HTTPRequestMetric(threading.Thread):
 
-    def __init__(self, metrics, port, timeout, **kwargs):
+    def __init__(self, result_q, q_timeout, sleep_time, port, timeout, **kwargs):
         super().__init__(**kwargs)
-        self.metrics = metrics
+        self.metrics = dict()
         self.m1 = Gauge(COMPUTER_STATE, "PC state", ["uid", "statename"])
         self.m2 = Gauge(HOST_UPTIME, "Host uptime", ["uid", "hostname", "ip", "domainname"])
         self.m3 = Gauge(USER_UPTIME, "User uptime", ["uid", "hostname", "ip", "domainname", "username"])
@@ -201,6 +157,11 @@ class HTTPRequestMetric(threading.Thread):
         self.m2_old = []
         self.m3_old = []
         self.state_off = {}
+        #
+        self.result_q = result_q
+        self.timeout = q_timeout
+        self._seconds = 0
+        self._sleep = sleep_time
 
     def stop(self):
         self._stopped = True
@@ -216,23 +177,57 @@ class HTTPRequestMetric(threading.Thread):
             self.m3.remove(*el)
         self.m3_old = []
 
+    #
+    def check_state(self):
+        for key in self.metrics:
+            if self.metrics[key]["state"] == STATE_OFF:
+                continue
+            if self.metrics[key]["state"] == STATE_UNKNOWN:
+                if datetime.datetime.now().timestamp() - self.metrics[key]["time_last_action"] > STATE_TIMEOUT_OFF:
+                    self.metrics[key]["state"] = STATE_OFF
+            else:
+                if datetime.datetime.now().timestamp() - self.metrics[key]["time_last_action"] > STATE_TIMEOUT_UNKNOWN:
+                    self.metrics[key]["state"] = STATE_UNKNOWN
+
+    #
+    def read_queue(self):
+        time.sleep(self._sleep)
+        self._seconds += self._sleep
+        if self._seconds > 60:
+            self._seconds = 0
+            self.check_state()
+        try:
+            data = self.result_q.get(block=True, timeout=self.timeout)
+            self.metrics[data["uid"]] = data.copy()
+            self.result_q.task_done()
+        except queue.Empty:
+            pass
+        except:
+            self.result_q.task_done()
+
+    #
+    def make_metrics(self):
+        for key, val in self.metrics.items():
+            self.m1.labels(key, STATE[val["state"]]).set(1)
+            self.m1_old.append((key, STATE[val["state"]]))
+            if val["state"] != STATE_OFF and self.state_off.get(key, 0) < STATE_TIMEOUT_UNKNOWN:
+                self.m2.labels(key, val["hostname"], val["ip"], val["domainname"]).set(val["host_uptime"])
+                self.m2_old.append((key, val["hostname"], val["ip"], val["domainname"]))
+                if val["username"]:
+                    self.m3.labels(key, val["hostname"], val["ip"], val["domainname"], val["username"]).set(
+                        val["user_uptime"])
+                    self.m3_old.append((key, val["hostname"], val["ip"], val["domainname"], val["username"]))
+                    self.state_off[key] = \
+                        self.state_off.get(key, 0) + self._timeout if val["state"] == STATE_UNKNOWN else 0
+            elif val["state"] == STATE_ON:
+                self.state_off[key] = 0
+
     def run(self):
         """Основной цикл обработки данных"""
         while not self._stopped:
+            self.read_queue()
             self.clear_metrics()
-            for key, val in self.metrics.items():
-                self.m1.labels(key, STATE[val["state"]]).set(1)
-                self.m1_old.append((key, STATE[val["state"]]))
-                if val["state"] != STATE_OFF and self.state_off.get(key, 0) < STATE_TIMEOUT_UNKNOWN:
-                    self.m2.labels(key, val["hostname"], val["ip"], val["domainname"]).set(val["host_uptime"])
-                    self.m2_old.append((key, val["hostname"], val["ip"], val["domainname"]))
-                    if val["username"]:
-                        self.m3.labels(key, val["hostname"], val["ip"], val["domainname"], val["username"]).set(val["user_uptime"])
-                        self.m3_old.append((key, val["hostname"], val["ip"], val["domainname"], val["username"]))
-                        self.state_off[key] = \
-                            self.state_off.get(key, 0) + self._timeout if val["state"] == STATE_UNKNOWN else 0
-                elif val["state"] == STATE_ON:
-                    self.state_off[key] = 0
+            self.make_metrics()
             time.sleep(self._timeout)
 
 
@@ -260,12 +255,7 @@ def main():
     thread_db.start()
     logging.info("Thread is started: {0}".format(thread_db.name))
 
-    metrics = dict()
-    thread_metrics = MakeMetricsWorker(metrics, result_queue, QUEUE_TIMEOUT, SLEEP_TIMEOUT, name="Thread metrics")
-    thread_metrics.start()
-    logging.info("Thread is started: {0}".format(thread_metrics.name))
-
-    client_prom = HTTPRequestMetric(metrics, PROMETHEUS_CLIENT_PORT, METRICS_TIMEOUT)
+    client_prom = HTTPRequestMetric(result_queue, QUEUE_TIMEOUT, SLEEP_TIMEOUT, PROMETHEUS_CLIENT_PORT, METRICS_TIMEOUT)
     client_prom.start()
     logging.info("Prometheus client is started")
 
@@ -279,10 +269,6 @@ def main():
     client_prom.stop()
     client_prom.join()
     logging.info("Prometheus client is stopped")
-
-    thread_metrics.stop()
-    thread_metrics.join()
-    logging.info("Thread is stopped: {0}".format(thread_metrics.name))
 
     thread_db.stop()
     thread_db.join()
